e0d08780012232d85f17dbb00dddfc237d8e6d1b
ephraim-jude <ephraimjude.t@gmail.com>
2025-04-27 13:59:26 +0000

    Changes to be committed: 	new file:   src/Components/ProjectsGrid.tsx 	new file:   src/Global.css 	modified:   src/Pages/Projects.tsx 	modified:   src/main.tsx

---
+++ src/Components/ProjectsGrid.tsx
@@ -0,0 +1,28 @@
+import { motion } from "motion/react";
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+   
+
+const ProjectGrid = ({ title, description, image, link }: ProjectProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      className="flex flex-col items-center justify-center bg-[#242038] text-[#CAC4CE] text-center font-Semibold font-montserrat"
+    >
+      <img src={image} alt={title} className="w-16 h-16 p-4" />
+      <h3 className="text-lg mt-2">{title}</h3>
+      <p className="text-sm mt-2">{description}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer" className="mt-2 text-blue-500 hover:text-blue-700">
+        View Project
+      </a>
+    </motion.div>
+  );
+}
+export default ProjectGrid;
--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,14 +1,14 @@
 import { motion } from "motion/react"
+import ProjectGrid from "../Components/ProjectsGrid";
 
 const Projects = () => {
     return (
-        <motion.div className=" bg-[#242038] bg-gradient-to-r from [#242038] to-[#665A9E] h-screen w-full flex flex-col items-center relative overflow-hidden z-[0]" style={{
+        <div className=" bg-[#242038] bg-gradient-to-r from [#242038] to-[#665A9E] h-screen w-full flex flex-col items-center relative overflow-hidden z-[0]" style={{
             animation: "gradient-bg 7s ease infinite",
             backgroundSize: "200% 200%",
           }}>
             <motion.div
             initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 0, scale: 0 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
             className="text-[#CAC4CE] text-2xl text-center font-bold font-montserrat z-[1] transition-opacity duration-500 ease-in-out
@@ -16,7 +16,24 @@ const Projects = () => {
             ">
                 Projects
             </motion.div>
-        </motion.div>
+            <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 0.5, scale: 1 }}
+                transition={{ duration: 0.5 }}
+                className="bg-[#242038] relative flex flex-col justify-center items-center rounded-xl z-[-1] h-[80vh] w-[80vw] top-[10vh] overflow-auto  ">
+                    <div className="mt-100 p-4 w-150">
+                        <ProjectGrid title="Stock Scraper" description="A full-stack stock market scraper built with FastAPI and React. It fetches top gaining and losing stocks using real-time APIs, and displays it beautifully in a dynamic dashboard. Includes powerful backend error handling and frontend search functionality for maximum usability." image="/path/to/image1.jpg" link="https://example.com/project1" />
+                    </div> 
+                    <div className="mt-25 p-4 w-150">
+                        <ProjectGrid title="Chatbot" description="An intelligent chatbot application using FastAPI and React. It integrates with OpenRouter's large language models to generate human-like responses. Features smooth frontend UX, clean backend API communication, and scalable design ready for business use cases." image="/path/to/image2.jpg" link="https://example.com/project2"  />
+                    </div> 
+                    <div className="mt-25 p-4 w-150">
+                        <ProjectGrid title="Portfolio" description="A modern and fast developer portfolio built with React and TailwindCSS. Showcases personal projects, skills, and contact details with a clean UI and mobile responsiveness. Focused on speed, minimalism, and strong first impressions for freelance clients and companies." image="/path/to/image3.jpg" link="https://example.com/project3" />
+                    </div>                 
+
+                    
+            </motion.div>
+        </div>
     )
 }
 
