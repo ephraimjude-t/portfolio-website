aba3867a729d88296f4bf50721f8b271decc9c71
ephraim-jude <ephraimjude.t@gmail.com>
2025-04-28 06:24:16 +0000

    final

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -22,13 +22,13 @@ const Projects = () => {
                 transition={{ duration: 0.5 }}
                 className="bg-[#242038] relative flex flex-col justify-center items-center rounded-xl z-[-1] h-[80vh] w-[80vw] top-[10vh] overflow-auto  ">
                     <div className="mt-300 p-4 w-150">
-                        <ProjectGrid title="Stock Scraper" description="A full-stack stock market scraper built with FastAPI and React. It fetches top gaining and losing stocks using real-time APIs, and displays it beautifully in a dynamic dashboard. Includes powerful backend error handling and frontend search functionality for maximum usability." image="./images/StockScraper.png" link="https://example.com/project1" />
+                        <ProjectGrid title="Stock Scraper" description="A full-stack stock market scraper built with FastAPI and React. It fetches top gaining and losing stocks using real-time APIs, and displays it beautifully in a dynamic dashboard. Includes powerful backend error handling and frontend search functionality for maximum usability." image="./images/StockScraper.png" link="https://github.com/ephraimjude-t/stock_scraper" />
                     </div> 
                     <div className="mt-25 p-4 w-150">
-                        <ProjectGrid title="Portfolio" description="A modern and fast developer portfolio built with React and TailwindCSS. Showcases personal projects, skills, and contact details with a clean UI and mobile responsiveness. Focused on speed, minimalism, and strong first impressions for freelance clients and companies." image="./images/Portfolio.png" link="https://example.com/project3" />
+                        <ProjectGrid title="Portfolio" description="A modern and fast developer portfolio built with React and TailwindCSS. Showcases personal projects, skills, and contact details with a clean UI and mobile responsiveness. Focused on speed, minimalism, and strong first impressions for freelance clients and companies." image="./images/Portfolio.png" link="https://github.com/ephraimjude-t/portfolio-website" />
                     </div>
                     <div className="mt-25 p-4 w-150">
-                        <ProjectGrid title="Chatbot" description="An intelligent chatbot application using FastAPI and React. It integrates with OpenRouter's large language models to generate human-like responses. Features smooth frontend UX, clean backend API communication, and scalable design ready for business use cases." image="./images/Chatbot.png" link="https://example.com/project2"  />
+                        <ProjectGrid title="Chatbot" description="An intelligent chatbot application using FastAPI and React. It integrates with OpenRouter's large language models to generate human-like responses. Features smooth frontend UX, clean backend API communication, and scalable design ready for business use cases." image="./images/Chatbot.png" link="https://github.com/ephraimjude-t/chatbot"  />
                     </div>                  
 
                     
